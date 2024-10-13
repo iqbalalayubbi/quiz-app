c292a195cdaf7a9974d6208234860843d74c59c5
iqbalalayubbi <iqbal.is415@gmail.com>
2024-10-13 18:20:51 +0700

    feat: direct user to login after click logout qa-15

--- src/pages/Quiz/libs/components/Navbar/Navbar.tsx
+++ src/pages/Quiz/libs/components/Navbar/Navbar.tsx
@@ -2,15 +2,34 @@ import {
   Flex,
   ClockCircleOutlined,
   LogoutOutlined,
+  Modal,
 } from "~/libs/components/components";
 import { type TimerContextType } from "~/libs/context/types";
-import { useContext } from "~/libs/hooks/hooks";
+import { useContext, useNavigate } from "~/libs/hooks/hooks";
 import { TimerContext } from "~/libs/context/contexts";
 
 import styles from "./styles.module.css";
+import { QuizStorage, TokenStorage } from "~/libs/storage/storage";
+import { AppRoute } from "~/libs/enums/enums";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
   const { displayTime } = useContext(TimerContext) as TimerContextType;
+
+  const handleLogout = () => {
+    Modal.confirm({
+      title: "Logout",
+      content: "Are you sure you want to logout?",
+      onOk: () => {
+        QuizStorage.removeResumeQuiz();
+        TokenStorage.removeToken();
+        navigate(AppRoute.LOGIN);
+      },
+      onCancel() {},
+      centered: true,
+    });
+  };
 
   return (
     <Flex className={styles["navbar"]} justify="space-between">
@@ -19,7 +38,12 @@ const Navbar = () => {
         <span>{displayTime}</span>
       </Flex>
       <h1 className={styles["title"]}>Quizea</h1>
-      <Flex className={styles["logout-container"]} align="center" gap={"8px"}>
+      <Flex
+        className={styles["logout-container"]}
+        align="center"
+        gap={"8px"}
+        onClick={handleLogout}
+      >
         <LogoutOutlined className={styles["logout-icon"]} />
         <span>Logout</span>
       </Flex>
